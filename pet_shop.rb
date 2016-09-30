f0cfc8dfeb6964bb88b0b11fbbe681dbbfd4a05e
Dol174 <david.oleary174@gmail.com>
2016-09-30 15:01:16 +0100

    third commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,5 +6,17 @@ def total_cash(sum)
   return @pet_shop[:admin][:total_cash]
 end
 
+def add_or_remove_cash(shop, newsum)
+  return shop[:admin][:total_cash] += newsum
+end
+
+def add_or_remove_cash(shop, newsum)
+  return shop[:admin][:total_cash] += newsum
+end
+
+def pets_sold(sum)
+  return @pet_shop[:admin][:pets_sold]
+
+end
 
 
